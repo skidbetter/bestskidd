20fc2de7f6b439200c84b3b129d108b5c724bbb4
R12sa <144062428+R12sa@users.noreply.github.com>
2026-06-05 17:39:38 -0700

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -45834,67 +45834,90 @@ end)
 --// END NERV8 MERGED MODULES
 
 run(function()
-	local BedProtector
-	
-	local function getBedNear()
-		local localPosition = entitylib.isAlive and entitylib.character.RootPart.Position or Vector3.zero
-		for _, v in collectionService:GetTagged('bed') do
-			if (localPosition - v.Position).Magnitude < 20 and v:GetAttribute('Team'..(lplr:GetAttribute('Team') or -1)..'NoBreak') then
-				return v
-			end
-		end
-	end
-	
-	local function getBlocks()
-		local blocks = {}
-		for _, item in store.inventory.inventory.items do
-			local block = bedwars.ItemMeta[item.itemType].block
-			if block then
-				table.insert(blocks, {item.itemType, block.health})
-			end
-		end
-		table.sort(blocks, function(a, b) 
-			return a[2] > b[2]
-		end)
-		return blocks
-	end
-	
-	local function getPyramid(size, grid)
-		local positions = {}
-		for h = size, 0, -1 do
-			for w = h, 0, -1 do
-				table.insert(positions, Vector3.new(w, (size - h), ((h + 1) - w)) * grid)
-				table.insert(positions, Vector3.new(w * -1, (size - h), ((h + 1) - w)) * grid)
-				table.insert(positions, Vector3.new(w, (size - h), (h - w) * -1) * grid)
-				table.insert(positions, Vector3.new(w * -1, (size - h), (h - w) * -1) * grid)
-			end
-		end
-		return positions
-	end
-	
-	BedProtector = vape.Categories.World:CreateModule({
-		Name = 'BedProtector',
-		Function = function(callback)
-			if callback then
-				local bed = getBedNear()
-				bed = bed and bed.Position or nil
-				if bed then
-					for i, block in getBlocks() do
-						for _, pos in getPyramid(i, 3) do
-							if not BedProtector.Enabled then break end
-							if getPlacedBlock(bed + pos) then continue end
-							bedwars.placeBlock(bed + pos, block[1], false)
-						end
-					end
-					if BedProtector.Enabled then 
-						BedProtector:Toggle() 
-					end
-				else
-					notif('BedProtector', 'Unable to locate bed', 5)
-					BedProtector:Toggle()
-				end
-			end
-		end,
-		Tooltip = 'Automatically places strong blocks around the bed.'
-	})
+    local BedProtector
+    local currentLayer = 0
+ 
+    local function getBedNear()
+        local localPosition = entitylib.isAlive and entitylib.character.HumanoidRootPart.Position or Vector3.zero
+        for _, v in pairs(collectionService:GetTagged('bed')) do
+            if (localPosition - v.Position).Magnitude < 20 and v:GetAttribute('Team'..(lplr:GetAttribute('Team') or -1)..'NoBreak') then
+                return v
+            end
+        end
+    end
+ 
+    local function getCurrentBlock()
+        local item = store.inventory.inventory.hand
+        if item then
+            local block = bedwars.ItemMeta[item.itemType].block
+            if block then
+                return item.itemType
+            end
+        end
+        return nil
+    end
+ 
+    local function getPyramid(size, grid)
+        local positions = {}
+        for h = size, 0, -1 do
+            for w = h, 0, -1 do
+                table.insert(positions, Vector3.new(w, (size - h), ((h + 1) - w)) * grid)
+                table.insert(positions, Vector3.new(w * -1, (size - h), ((h + 1) - w)) * grid)
+                table.insert(positions, Vector3.new(w, (size - h), (h - w) * -1) * grid)
+                table.insert(positions, Vector3.new(w * -1, (size - h), (h - w) * -1) * grid)
+            end
+        end
+        return positions
+    end
+ 
+    local function findBrokenPositions(bed)
+        local broken = {}
+        for i = 0, currentLayer do
+            for _, pos in pairs(getPyramid(i, 3)) do
+                if not getPlacedBlock(bed.Position + pos) then
+                    table.insert(broken, pos)
+                end
+            end
+        end
+        return broken
+    end
+ 
+    BedProtector = vape.Categories.World:CreateModule({
+        Name = 'BedProtector',
+        Function = function(callback)
+            if callback then
+                local bed = getBedNear()
+                if bed then
+                    local blockType = getCurrentBlock()
+                    if blockType then
+                        local brokenPositions = findBrokenPositions(bed)
+                        if #brokenPositions > 0 then
+                            for _, pos in pairs(brokenPositions) do
+                                if not BedProtector.Enabled then break end
+                                bedwars.placeBlock(bed.Position + pos, blockType)
+                                task.wait(0.01)
+                            end
+                        else
+                            currentLayer = currentLayer + 1
+                            for _, pos in pairs(getPyramid(currentLayer, 3)) do
+                                if not BedProtector.Enabled then break end
+                                bedwars.placeBlock(bed.Position + pos, blockType)
+                                task.wait(0.01)
+                            end
+                        end
+                        if BedProtector.Enabled then 
+                            BedProtector:Toggle() 
+                        end
+                    else
+                        notif('BedProtector', 'Hold blocks in your hand', 5)
+                        BedProtector:Toggle()
+                    end
+                else
+                    notif('BedProtector', 'Get closer to your bed', 5)
+                    BedProtector:Toggle()
+                end
+            end
+        end,
+        Tooltip = 'Creates perfect bed protection layers'
+    })
 end)
